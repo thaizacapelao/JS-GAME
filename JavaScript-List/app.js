2b37fa1bccdef9bb477040f12c896ac4924e5594
Thaiza Capelão <thaizacapelao@gmail.com>
2024-05-29 16:48:15 -0400

    Adicionando interacao ao botao de chute

--- JavaScript-List/app.js
+++ JavaScript-List/app.js
@@ -3,3 +3,7 @@ titulo.innerHTML = 'Jogo do Número Secreto';
 
 let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+function verificarChute() {
+    console.log('O botão foi clicado');
+}
