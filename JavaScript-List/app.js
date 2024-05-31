c566004c9af6866a4d5f514c5321cb27595e6a03
Thaiza Capelão <thaizacapelao@gmail.com>
2024-05-31 12:22:24 -0400

    alterando funcao do h1 e p

--- JavaScript-List/app.js
+++ JavaScript-List/app.js
@@ -1,8 +1,20 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do Número Secreto';
+// let titulo = document.querySelector('h1');
+// titulo.innerHTML = 'Jogo do Número Secreto';
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+// let paragrafo = document.querySelector('p');
+// paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+//Fazendo uma função para que nao seja necessario escrever duas linhas de código toda vez
+// que for alterar uma tag e um texto apenas
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do Número Secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+
 
 function verificarChute() {
     console.log('O botão foi clicado');
