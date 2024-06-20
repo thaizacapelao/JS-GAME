44a842044c53d705e9fe949187c14c3094db2c88
Thaiza Capelão <thaizacapelao@gmail.com>
2024-06-20 18:01:05 -0400

    Adicionando listas e fatorial

--- JavaScript-List/exercise.js
+++ JavaScript-List/exercise.js
@@ -177,3 +177,19 @@
 
 // const numero = 7; 
 // tabuada(numero);
+
+//Crie uma lista vazia, com o nome listaGenerica.
+// Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+// Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+// Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+// Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+// Crie uma lista com 3 nomes e exiba no console apenas o último elemento:
+
+// let listaGenerica = [];
+// let linguagensDeProgramacao = ["JavaScript", "C", "C++", "Kotlin", "Pyton"];
+// linguagensDeProgramacao.push("Java", "Ruby", "GoLang")
+// console.log(linguagensDeProgramacao);
+// let lista3Nomes = ["Eu", "Tu", "Ele"];
+// console.log(lista3Nomes[0]);
+// console.log(lista3Nomes[1]);
+// console.log(lista3Nomes[2]);
